ea00af8a83d6482a3f174a94c51c633e74be377f
illia <nudrob557@gmail.com>
2025-07-31 11:57:31 +0300

    fix controller for sign up devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:phone, :email, :password, :password_confirmation, :remember_me, :name]
+    added_attrs = [:phone, :email, :first_name, :last_name, :password, :password_confirmation, :remember_me, :name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
